facc2b43681ea6631f9b71deb8b08b52dbedb6ab
tim <tim@ibears.io>
2024-06-18 17:57:14 +0800

    feat: add axios lib and finish get hero list

--- config/default.js
+++ config/default.js
@@ -1,3 +1,4 @@
 module.exports = {
-    PORT: 3000
+    PORT: 3000,
+    API_BASE_URL: 'https://hahow-recruit.herokuapp.com',
 }
--- service/hero.js
+++ service/hero.js
@@ -1,18 +1,7 @@
-async function getHeroesList(){
-    return {
-        heroes: [
-          {
-            id: 1,
-            name: "Daredevil",
-            image: "http://i.annihil.us/u/prod/marvel/i/mg/6/90/537ba6d49472b/standard_xlarge.jpg"
-          },
-          {
-            id: 2,
-            name: "Thor",
-            image: "http://x.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg"
-          },
-        ]
-      }
+const { sendGetRequestToHahowAPI } = require('../lib/axios')
+
+function getHeroesList(){
+    return sendGetRequestToHahowAPI('/heroes')
 }
 
 module.exports = {
