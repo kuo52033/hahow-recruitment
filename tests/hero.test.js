a8d32822e572af1a405cfd7cc20cf7029e05c786
tim <tim@ibears.io>
2024-06-21 15:04:09 +0800

    test: add get hero test

--- tests/hero.test.js
+++ tests/hero.test.js
@@ -1,7 +1,15 @@
 const request = require('supertest');
 const { app } = require('../src/app');
-const { AuthenticationError } = require('../src/lib/error');
-const { AUTHENTICATED_ERROR } = require('../src/lib/error/code');
+const { 
+	AuthenticationError,
+	NotFoundError,
+	RequestValidationError,
+ } = require('../src/lib/error');
+const { 
+	AUTHENTICATED_ERROR,
+	NOT_FOUND_HERO,
+	INVALID_REQUEST_PAYLOAD,
+ } = require('../src/lib/error/code');
 
 const validUser = { Name: 'hahow', Password: 'rocks' }
 const invalidUser = { Name: 'hahow', Password: 'rockssss' }
@@ -59,6 +67,88 @@ describe('hero API - get /api/v1/heroes', () => {
 			.expect(401)
 			.then(res => {
 				const error = new AuthenticationError(AUTHENTICATED_ERROR);
+
+				expect(res.body).toMatchObject({
+					type: error.constructor.name,
+					message: error.message,
+					code: error.code
+				});
+			});	
+	});
+});
+
+describe('hero API - get /api/v1/heroe/id=:heroId', () => {
+	test('200 ok - should return hero', () => {
+		return request(app)
+			.get('/api/v1/heroes/id=3')
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.expect(200)
+			.then(res => {
+				expect(res.body).toMatchObject(heroMatch);
+			});	
+	});
+
+	test('200 ok - should return hero with authenticated profile', () => {
+		return request(app)
+			.get('/api/v1/heroes/id=3')
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.set(validUser)
+			.expect(200)
+			.then(res => {
+				expect(res.body).toMatchObject({
+					...heroMatch,
+					...heroProfileMatch,
+				});
+			});	
+	});
+
+	test('401 unauthenticated - invalid Name or Password', () => {
+		return request(app)
+			.get('/api/v1/heroes/id=3')
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.set(invalidUser)
+			.expect(401)
+			.then(res => {
+				const error = new AuthenticationError(AUTHENTICATED_ERROR);
+
+				expect(res.body).toMatchObject({
+					type: error.constructor.name,
+					message: error.message,
+					code: error.code
+				});
+			});	
+	});
+
+	test('404 not found - not found hero', () => {
+		return request(app)
+			.get('/api/v1/heroes/id=800')
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.set(validUser)
+			.expect(404)
+			.then(res => {
+				const error = new NotFoundError(NOT_FOUND_HERO);
+
+				expect(res.body).toMatchObject({
+					type: error.constructor.name,
+					message: error.message,
+					code: error.code
+				});
+			});	
+	});
+
+	test('422 unprocessable entity - invalid request payload', () => {
+		return request(app)
+			.get('/api/v1/heroes/id=test')
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.set(validUser)
+			.expect(422)
+			.then(res => {
+				const error = new RequestValidationError(INVALID_REQUEST_PAYLOAD);
 
 				expect(res.body).toMatchObject({
 					type: error.constructor.name,
