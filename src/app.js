c8364fc2c452c61cd611c252af73328e69a2ada4
tim <tim@ibears.io>
2024-06-22 23:45:09 +0800

    refactor: server listening split into index.js file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const config = require('config');
 const router = require('./routes');
 const handleError = require('./middlewares/handle-error');
 const logRequest = require('./middlewares/log-request');
@@ -9,9 +8,5 @@ const app = express();
 app.use(logRequest);
 app.use('/api/v1', router);
 app.use(handleError);
-
-app.listen(config.PORT, () => {
-    console.log(`server is listening on port ${config.PORT}`);
-})
 
 module.exports = { app }
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const config = require('config');
+const { app } = require('./app');
+
+app.listen(config.PORT, () => {
+    console.log(`server is listening on port ${config.PORT}`);
+})
